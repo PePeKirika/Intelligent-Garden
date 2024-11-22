eb37016044f77e204c0519f808c9cd763ff07d4b
PePeKirika <nongpepe@gmail.com>
2024-11-22 17:56:50 +0700

    try web speech api

--- Frontend/intelligent_garden/src/app/page.tsx
+++ Frontend/intelligent_garden/src/app/page.tsx
@@ -1,8 +1,12 @@
 'use client'
-import { database } from "../app/lib/firebase";
+import { database } from "./components/firebase";
 import { ref, set, onValue, child } from "firebase/database";
 import { useState, useEffect } from "react";
-import { appendData } from "../app/lib/google-sheets.action";
+import { appendData } from "./components/google-sheets.action";
+import SpeechToText from "./components/speech-to-text";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { faSave } from "@fortawesome/free-solid-svg-icons";
 
 
 const Home = () => {
@@ -43,12 +47,45 @@ const handleOnSheetDataClick = async () => {
   }
 };
 
+const handleSpeechToText = (text : string) => {
+  if (text.toLocaleLowerCase().includes("ight") || text.includes("แสง")) {
+    textToSpeech("The light value is " + light);
+    alert("The light value is " + light);
+  } else if(text.toLocaleLowerCase().includes("temperature") || text.includes("อุณหภูมิ")) {
+    textToSpeech("The temperature is " + temperature + "degrees Celsius");
+    alert("The temperature is " + temperature + "°C");
+  } else if(text.toLocaleLowerCase().includes("time") || text.includes("เวลา")) {
+    textToSpeech("The timestamp is " + timestamp);
+    alert("The timestamp is " + timestamp);
+  } else if (text.toLocaleLowerCase().includes("soil") || text.includes("ดิน")) {
+    textToSpeech(`The soil moisture value of pot 1 is ${100 - pots[1].soil_moisture} percent`);
+    alert(`The soil moisture value of pot 1 is ${100 - pots[1].soil_moisture}%`);
+  } else if (text.toLocaleLowerCase().includes("pump") || text.includes("ปั๊ม")) {
+    textToSpeech(`The pump state of pot 1 is ${pots[1].pump ? "ON" : "OFF"}`);
+    alert(`The pump state of pot 1 is ${pots[1].pump ? "ON" : "OFF"}`);
+  } else if(text.toLocaleLowerCase().includes("humidity") || text.includes("ชื้น")) {
+    textToSpeech("The humidity is " + humidity);
+    alert("The humidity is " + humidity);
+  } else {
+    alert("Sorry, I didn't get that. Please try again.");
+  }
+}
+
+const textToSpeech = (text : string) => {
+  const synth = window.speechSynthesis;
+  const utterance = new SpeechSynthesisUtterance(text);
+  synth.speak(utterance);
+}
+
   return (
     <main className="font-sans bg-gradient-to-br from-indigo-900 via-red-700 to-yellow-400 text-gray-800 min-h-screen leading-relaxed antialiased overflow-x-hidden">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
       <div id="particles-js" className="fixed top-0 left-0 w-full h-full z-10"></div>
 
       <div className="content relative z-20 p-5 flex flex-col items-center min-h-screen">
+        
+        
+      <SpeechToText handleSpeechToText = {handleSpeechToText}/>
         <div id="main-system" className="w-full text-center">
           <h1 className="mb-8 mt-5 text-white text-6xl font-bold text-shadow-md">
             Intelligent Garden
@@ -108,7 +145,7 @@ const handleOnSheetDataClick = async () => {
 
                 <div className="card flex flex-col justify-center items-center p-5 rounded-full bg-white bg-opacity-90 backdrop-blur-lg shadow-lg transform transition-transform duration-300 mt-4 mx-20">
                   <h2 className="text-lg font-semibold text-blue-500">
-                    Pump State <i className="fas fa-pump"></i>
+                    Pump State
                   </h2>
                   <p className="text-xl text-gray-600 font-semibold">
                     <span id={`pump_${index}`}>{pot.pump ? "ON" : "OFF"}</span>
@@ -120,11 +157,12 @@ const handleOnSheetDataClick = async () => {
         </div>
 
         <button
-          className="mt-8 p-4 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 focus:outline-none"
-          onClick={handleOnSheetDataClick}
-        >
-          Save to Google Sheet
+          className="mt-8 p-4 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 focus:outline-none w-fit-content flex items-center gap-2"
+          onClick={handleOnSheetDataClick}>
+            <p >Save to Google Sheet</p>
+            <FontAwesomeIcon icon={faSave}/>
         </button>
+
       </div>
     </main>
 
